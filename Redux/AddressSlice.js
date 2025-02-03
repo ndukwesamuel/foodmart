169d4756848e53b9ba70c79b5a3d435fc7f6bc39
Eniola <opakunleeniolail2019@gmail.com>
2025-02-03 13:08:42 +0100

    feat:  create get_all_addresses function

---
+++ Redux/AddressSlice.js
@@ -0,0 +1,99 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+import axios from "axios";
+
+import Toast from "react-native-toast-message";
+import { handleApiError } from "./shareApi";
+import { axiosInstance, getAxiosConfig, getToken } from "./ApiConfig";
+
+const API_BASEURL = process.env.EXPO_PUBLIC_API_URL;
+
+const initialState = {
+  Get_all_addresses_data: null,
+  Get_all_addresses_isError: false,
+  Get_all_addresses_isSuccess: false,
+  Get_all_addresses_isLoading: false,
+  Get_all_addresses_message: null,
+
+  Get_an_address_data: null,
+  Get_an_address_isError: false,
+  Get_an_address_isSuccess: false,
+  Get_an_address_isLoading: false,
+  Get_an_address_message: null,
+};
+
+export const Get_all_addresses = createAsyncThunk(
+  "AddressSlice/Get_all_addresses",
+  async (data, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState()?.Auth?.user_data?.data?.token;
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.get(
+        `${API_BASEURL}v1/customer/addresses`,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "An error occurred"
+      );
+    }
+  }
+);
+
+export const Get_an_address = createAsyncThunk(
+  "AddressSlice/Get_an_address",
+  async (data, thunkAPI) => {
+    try {
+      const token = thunkAPI.getState()?.Auth?.user_data?.data?.token;
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+      const response = await axios.get(
+        `${API_BASEURL}v1/customer/addresses/:${data}`,
+        config
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(
+        error.response?.data?.message || "An error occurred"
+      );
+    }
+  }
+);
+
+export const AddressSlice = createSlice({
+  name: "AddressSlice",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(Get_all_addresses.pending, (state) => {
+        state.Get_all_addresses_isLoading = true;
+      })
+      .addCase(Get_all_addresses.fulfilled, (state, action) => {
+        state.Get_all_addresses_isLoading = false;
+        state.Get_all_addresses_isError = false;
+        state.Get_all_addresses_data = action.payload;
+        state.Get_all_addresses_message = null;
+        state.Get_all_addresses_isSuccess - true;
+      })
+      .addCase(Get_all_addresses.rejected, (state, action) => {
+        state.Get_all_addresses_isLoading = false;
+        state.Get_all_addresses_isError = true;
+        state.Get_all_addresses_message = action.payload;
+        state.Get_all_addresses_data = null;
+        state.Get_all_addresses_isSuccess = false;
+      });
+  },
+});
